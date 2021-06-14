df2e9dff1e52bcb3737450c0c596f6b149e44ad4
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-06-14 23:08:46 +0530

    added error control

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,14 +5,39 @@ import { apiGet } from '../misc/config';
 function Show() {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoad, setIsLoad] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            setShow(results);
+            setIsLoad(false);
+          }
+        }, 2000);
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.msg);
+          setIsLoad(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log(show);
+
+  if (isLoad) {
+    return <div>Data is being loaded.</div>;
+  }
+  if (error) {
+    return <div>Error occured:{error}</div>;
+  }
   return <div>This is the show page</div>;
 }
 
