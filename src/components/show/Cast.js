4c9be3ac9acd3c45063d9936f7a5307658daf373
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-06-15 23:10:47 +0530

    aded files

---
+++ src/components/show/Cast.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Cast() {
+  return <div></div>;
+}
+
+export default Cast;
---
+++ src/components/show/ShowMainData.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ShowMainDetails() {
+  return <div></div>;
+}
+
+export default ShowMainDetails;
