476425c6215550d2b1e16f3e6f11c410a246c8b4
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-06-17 21:27:44 +0530

    completed functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,10 @@ import MainPageLayout from '../components/MainPageLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import ActorGrid from '../components/actor/ActorGrid';
 import { apiGet } from '../misc/config';
+import { useLastQuery } from '../misc/custom-hooks';
 
 function Home() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery('');
   const [results, setResults] = useState(null);
   const [searchOpt, setSearchOpt] = useState('shows');
 
