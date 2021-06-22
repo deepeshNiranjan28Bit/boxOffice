3ef905de382599a4b69717b9c6dedb30422cdd8a
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-06-22 21:36:34 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <HashRouter>
@@ -21,4 +20,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
