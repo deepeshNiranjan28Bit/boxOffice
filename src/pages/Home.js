4bcc39863023c44cd74b27f876e7e25af291639b
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-06-23 21:43:50 +0530

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,8 +29,8 @@ const renderResults = results => {
 
 const Home = () => {
   const [input, setInput] = useLastQuery();
-  const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
+  const [results, setResults] = useState(null);
 
   const isShowsSearch = searchOption === 'shows';
   const onSearch = () => {
